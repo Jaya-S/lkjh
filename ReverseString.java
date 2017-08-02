6b887f666f797fa24de10c62b145a5c84196a2ac
Jaya-S <jaivedgiri@gmail.com>
2017-08-02 10:08:46 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+# lkjh
+import java.io.*;
+
+public class ReverseString {
+ 	public static void main(String args[]){
+   	
+  		String String="jayashree";
+  	
+    		String reverse=new StringBuffer(String).reverse().toString();
+    	
+  		
+  		System.out.println("\n String before reverse:"+String);
+  		System.out.println("String after reverse:"+reverse);
+  		
+ 	}
+  
+}
